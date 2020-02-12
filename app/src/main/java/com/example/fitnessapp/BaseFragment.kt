1fe1a629d72fb4b4aeb734093ada11b8063c668c
Martynas <martynas358@gmail.com>
2020-02-12 18:15:46 +0000

    implementing example 1 calendar

---
+++ app/src/main/java/com/example/fitnessapp/BaseFragment.kt
@@ -0,0 +1,6 @@
+package com.example.fitnessapp
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+//import kotlinx.android.synthetic.main.home_activity.*
--- app/src/main/java/com/example/fitnessapp/TrackMenstrualCycleActivity.kt
+++ app/src/main/java/com/example/fitnessapp/TrackMenstrualCycleActivity.kt
@@ -7,109 +7,49 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.children
 import com.events.calendar.utils.EventsCalendarUtil
 import com.events.calendar.views.EventsCalendar
 import com.events.calendar.utils.EventsCalendarUtil.getDateString
-//import com.events.calendarsample.FontsManager
+import com.jakewharton.threetenabp.AndroidThreeTen
 import kotlinx.android.synthetic.main.activity_track_menstrual_cycle.*
+import kotlinx.android.synthetic.main.calendar_day_legend.*
+import org.threeten.bp.LocalDate
+import org.threeten.bp.YearMonth
+import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.format.TextStyle
 import java.util.*
 
-class TrackMenstrualCycleActivity: AppCompatActivity(), EventsCalendar.Callback {
+class TrackMenstrualCycleActivity: AppCompatActivity() {
 
-    override fun onDayLongPressed(selectedDate: Calendar?) {
-        Log.d("LONG CLICKED", EventsCalendarUtil.getDateString(selectedDate, EventsCalendarUtil.DD_MM_YYYY))
-    }
-
-    override fun onMonthChanged(monthStartDate: Calendar?) {
-        Log.d("MON", "CHANGED")
-    }
-
-    override fun onDaySelected(selectedDate: Calendar?) {
-        Log.d("CLICKED", EventsCalendarUtil.getDateString(selectedDate, EventsCalendarUtil.DD_MM_YYYY))
-    }
+    private val selectedDates = mutableSetOf<LocalDate>()
+    private val today = LocalDate.now()
+    private val monthTitleFormatter = DateTimeFormatter.ofPattern("MMMM")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AndroidThreeTen.init(this)
+
         setContentView(R.layout.activity_track_menstrual_cycle)
 
-//        selected.text = getDateString(eventsCalendar.getCurrentSelectedDate()?.timeInMillis)
-
-        val today = Calendar.getInstance()
-        val start = Calendar.getInstance()
-        start.add(Calendar.MONTH, -3)
-
-        val end = Calendar.getInstance()
-        end.add(Calendar.YEAR, 2)
-
-        eventsCalendar.setSelectionMode(eventsCalendar.MULTIPLE_SELECTION)
-            .setToday(today)
-            .setMonthRange(start, end)
-            .setWeekStartDay(Calendar.SUNDAY, false)
-            .setCurrentSelectedDate(today)
-            .setIsBoldTextOnSelectionEnabled(true)
-//            .setDatesTypeface(FontsManager.getTypeface(FontsManager.OPENSANS_REGULAR, this))
-//            .setMonthTitleTypeface(FontsManager.getTypeface(FontsManager.OPENSANS_SEMIBOLD, this))
-//            .setWeekHeaderTypeface(FontsManager.getTypeface(FontsManager.OPENSANS_SEMIBOLD, this))
-            .setCallback(this)
-            .build()
-
-        val c = Calendar.getInstance()
-        c.add(Calendar.DAY_OF_MONTH, 2)
-        eventsCalendar.addEvent(c)
-        c.add(Calendar.DAY_OF_MONTH, 3)
-        eventsCalendar.addEvent(c)
-        c.add(Calendar.DAY_OF_MONTH, 4)
-        eventsCalendar.addEvent(c)
-        c.add(Calendar.DAY_OF_MONTH, 7)
-        eventsCalendar.addEvent(c)
-        c.add(Calendar.MONTH, 1)
-        c[Calendar.DAY_OF_MONTH] = 1
-        eventsCalendar.addEvent(c)
-
-//        selected.setOnClickListener {
-//            val dates = eventsCalendar.getDatesFromSelectedRange()
-//            Log.e("SELECTED SIZE", dates.size.toString())
-//        }
-
-//        selected.typeface = FontsManager.getTypeface(FontsManager.OPENSANS_SEMIBOLD, this)
-
-        val dc = Calendar.getInstance()
-        dc.add(Calendar.DAY_OF_MONTH, 2)
-
-        activity_track_menstrual_cycle_button_save.setOnClickListener {
-            Log.d("TrackMCycleActivity", "Save btn has been clicked")
-
-            val selectedDates = eventsCalendar.getDatesFromSelectedRange();
-            val intent = Intent(this, VIewTrainingAdviceActivity::class.java)
-
-            // Can't pass Calendar object to intent, convert to milliseconds instead
-            intent.putExtra("SELECTED_DATES_LAST", selectedDates.last().timeInMillis)
-            startActivity(intent)
-        }
-    }
-
-    private fun getDateString(time: Long?): String {
-        if (time != null) {
-            val cal = Calendar.getInstance()
-            cal.timeInMillis = time
-            val month = when (cal[Calendar.MONTH]) {
-                Calendar.JANUARY -> "January"
-                Calendar.FEBRUARY -> "February"
-                Calendar.MARCH -> "March"
-                Calendar.APRIL -> "April"
-                Calendar.MAY -> "May"
-                Calendar.JUNE -> "June"
-                Calendar.JULY -> "July"
-                Calendar.AUGUST -> "August"
-                Calendar.SEPTEMBER -> "September"
-                Calendar.OCTOBER -> "October"
-                Calendar.NOVEMBER -> "November"
-                Calendar.DECEMBER -> "December"
-                else -> ""
+        val daysOfWeek = daysOfWeekFromLocale()
+        legendLayout.children.forEachIndexed { index, view ->
+            (view as TextView).apply {
+                text = daysOfWeek[index].getDisplayName(TextStyle.SHORT, Locale.ENGLISH).toUpperCase(Locale.ENGLISH)
+                setTextColorRes(R.color.example_1_white_light)
             }
-            return "$month ${cal[Calendar.DAY_OF_MONTH]}, ${cal[Calendar.YEAR]}"
-        } else return ""
+        }
+
+        val currentMonth = YearMonth.now()
+        val startMonth = currentMonth.minusMonths(3)
+        val endMonth = currentMonth.plusMonths(1)
+//        exOneCalendar.setup(startMonth, endMonth, daysOfWeek.first())
+//        exOneCalendar.scrollToMonth(currentMonth)
+
+
     }
 }
